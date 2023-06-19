ceeb3ce386605aaa1a6d002bfdc3518ab17dd705
Dipankaj Singh <dipankajsingh2@gmail.com>
2023-06-19 07:45:24 +0530

    fixed server issue

--- index.js
+++ index.js
@@ -6,60 +6,114 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const { MongoClient } = require('mongodb');
 const bodyParser = require('body-parser');
+
 app.use(bodyParser.json());
-app.use(require('cors')())
+app.use(require('cors')());
+
+const rooms = {};
+
+// Serve the index.html file
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-// connecting to mongoDb
-const mongoClient = new MongoClient("mongodb+srv://dipankajg1:motu_patalu@quickchat.stzv5i8.mongodb.net/?retryWrites=true&w=majority");
-
-// function to get recent messages
-async function getMessages(room) {
-    // sapreting room and user name 
-    const roomName = room.split(" ")[0]
-    await mongoClient.connect();
-    const db = mongoClient.db('quickchat');
-    const userChats = db.collection('usersChat');
-    // fetching the recent messages
-    const recentMessages = await userChats.findOneAndUpdate({ "roomKey": roomName }, { $setOnInsert: { "roomKey": roomName, "chats": [] } }, { upsert: true })
-    return recentMessages.value.chats
+// Connect to MongoDB
+async function connectToMongoDB() {
+    try {
+        const mongoClient = new MongoClient("mongodb+srv://dipankajg1:motu_patalu@quickchat.stzv5i8.mongodb.net/?retryWrites=true&w=majority");
+        await mongoClient.connect();
+        console.log('Connected to MongoDB');
+        return mongoClient;
+    } catch (err) {
+        console.error('Failed to connect to MongoDB:', err);
+        throw err;
+    }
 }
 
-async function pushMessage(message, userName, roomKey, messageId, time) {
-    const messageFrame = { userName, message, messageId, time: time }
-
-    roomKey = roomKey.split(' ')[0]
-    await mongoClient.connect();
-    const db = mongoClient.db('quickchat');
+// Function to get recent messages
+async function getMessages(room, db) {
+    const roomName = room.split(" ")[0];
     const userChats = db.collection('usersChat');
-    const oldMessage = await userChats.findOne({ "roomKey": roomKey.split(' ')[0] })
-    let returnVal = await userChats.updateOne({ "roomKey": roomKey }, {
-        $set: { "chats": [...oldMessage.chats, messageFrame] }
-    }, { upsert: true })
-    return returnVal
+    const recentMessages = await userChats.findOneAndUpdate(
+        { roomKey: roomName },
+        { $setOnInsert: { roomKey: roomName, chats: [] } },
+        { upsert: true, returnOriginal: false }
+    );
+    return recentMessages.value ? recentMessages.value.chats : [];
 }
 
+// Function to push a new chat message
+async function pushMessage(message, userName, roomKey, messageId, time, db) {
+    try {
+        const messageFrame = { userName, message, messageId, time };
+        roomKey = roomKey.split(' ')[0];
+        const userChats = db.collection('usersChat');
+        const oldMessage = await userChats.findOne({ roomKey: roomKey });
+        const returnVal = await userChats.updateOne(
+            { roomKey: roomKey },
+            { $set: { chats: [...oldMessage.chats, messageFrame] } },
+            { upsert: true }
+        );
+        return returnVal;
+    } catch (err) {
+        console.error('Failed to push message:', err);
+        throw err;
+    }
+}
 
 io.on('connection', (socket) => {
     // Join a room
     socket.on('joinRoom', async (room) => {
-        // create room if not exists already
-        const roomName = room.split(" ")[0]
-        const userName = room.split(" ")[1]
+        const roomName = room.split(" ")[0];
+        if (!rooms.hasOwnProperty(roomName)) {
+            rooms[roomName] = [socket.id]; // Create a new room array if it doesn't exist
+        } else {
+            rooms[roomName].push(socket.id);
+        }
         socket.join(roomName);
-        const recentMessages = await getMessages(room)
-        io.to(roomName).emit('fetchRecentChats', recentMessages)
-        // console.log(recentMessages.chats.time)
+        [].push()
+        // Retrieve the MongoDB client from the global scope
+        const db = mongoClient.db('quickchat');
+
+        const recentMessages = await getMessages(room, db);
+
+        io.to(roomName).except(() => { if (rooms[roomName].length !== 0) return rooms[roomName][0] }).emit('fetchRecentChats', recentMessages);
+        console.log(rooms);
     });
+
+    socket.on('disconnect', () => {
+        Object.keys(rooms).forEach((room) => {
+            const index = rooms[room].indexOf(socket.id);
+            if (index !== -1) {
+                rooms[room].splice(index, 1);
+            }
+        });
+        console.log(rooms);
+    });
+
     // Handle chat message
     socket.on('chatMessage', async (data) => {
-        // Broadcast the message to all clients in the same room
-        io.to(data.room.split(' ')[0].toLowerCase()).except(socket.id).emit('chatMessage', data);
-        await pushMessage(data.message, data.userName, data.room, data.messageId, data.time)
+        // Broadcast the message to all clients in the same room, excluding the sender
+        socket.to(data.room.split(' ')[0].toLowerCase()).emit('chatMessage', data);
+
+        // Retrieve the MongoDB client from the global scope
+        const db = mongoClient.db('quickchat');
+
+        await pushMessage(data.message, data.userName, data.room, data.messageId, data.time, db);
     });
 });
-server.listen(3000, () => {
-    console.log('server started on port 3000');
+
+// Start the server and connect to MongoDB
+const PORT = 3000;
+const mongoClientPromise = connectToMongoDB();
+
+mongoClientPromise.then((mongoClient) => {
+    // Store the MongoDB client in the global scope for later use
+    global.mongoClient = mongoClient;
+
+    server.listen(PORT, () => {
+        console.log(`Server started on port ${PORT}`);
+    });
+}).catch((err) => {
+    console.error('Failed to start the server:', err);
 });
